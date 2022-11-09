2e58a940fc7536e52988b0d9326e83eaffe94a35
gauravbasantani <gauravbasantani2525@gmail.com>
2022-11-09 20:12:21 +0530

    admin crud done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
 import Dashboard from "./components/Dashboard";
 
+import Admin from "./components/Admin";
 import Trainers from "./components/Trainers/Trainers";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <Header />
     <Routes>
       <Route path="/" element={<Dashboard/>}/>
+      <Route path="admins" element={<Admin/>}/>
       <Route path="trainers" element={<Trainers/>}/> 
     </Routes>
     
---
+++ src/components/Admin.js
@@ -0,0 +1,210 @@
+
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Col, Container, Row, Table } from 'react-bootstrap';
+import { Link, NavLink } from 'react-router-dom'
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
+const Admin = () => {
+    let [datas, setDatas] = useState([]);
+  let [data, setData] = useState({
+    _id:'',
+    name:'',
+    email:'',
+    mobileno:'',
+    website:'',
+    password:'',
+    status:'',
+    tagline:'',
+    gatewayid:''
+  });
+  const [show, setShow] = useState(false);
+ 
+  const handleClose = () => {
+    setShow(false);
+    load();
+  };
+  const handleShow = (e, d) => {
+    if(d==={}){
+      setData({});
+    }else{
+      setData(d);
+    }
+    setShow(true);
+  }
+  function load(){
+    axios.get('http://localhost:8081/admin/admin').then((res)=>{
+      console.log(res.data);
+      setDatas(res.data.data)
+    })
+  }
+  useEffect(()=>{
+    load();
+  },[]);
+  const handleChange = (e) =>{
+    const newData = {...data}
+    newData[e.target.id] = e.target.value
+    setData(newData)
+  }
+  function handleSubmit(e){
+    e.preventDefault();
+    let body = {
+      id:data._id,
+      name:data.name,
+      email:data.email,
+      password:data.password,
+      
+    };
+    console.log(body);
+    if(data._id === undefined){
+    axios.put('http://localhost:8081/admin/admin',body).then((res)=>{
+      console.log(res.data.data);
+      handleClose(true);
+    })
+  }
+  else{
+    axios.post('http://localhost:8081/admin/admin',body).then((res)=>{
+      console.log(res.data.data);
+      handleClose(true);
+    })
+  }
+  }
+  function deletetrainer(e, id){
+    e.preventDefault();
+    axios.delete("http://localhost:8081/admin/admin", {data:{id:id}}).then((res)=>{
+      load();
+    })
+  }
+  return (
+    <div>
+     <div className='container'>
+      <div className='breadcrumbs'>
+        <p className='bread'>
+          <NavLink to='/'><span Link to='/'>  Admin </span></NavLink>/
+          <span >Admin</span> 
+          </p>
+      </div>
+      <h1>Admin</h1>
+      <div className='text-right'>
+      <Button  className='btn btn-primary' onClick={(e)=>{handleShow(e, {})}}>Add</Button>
+      </div>
+      <div className='container mt-3'>
+      <Table responsive class="table table-bordered bordered table-stripped">
+        <thead>
+          <tr>
+          <th>Action</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Password</th>         
+          </tr>
+        </thead>
+        <tbody>
+        {
+          datas.map((d)=>{
+            return(
+              <tr className='mx-5' key={ d._id }>
+             
+                <td className='px-3 mx-3'>
+                  <button className="btn btn-primary " onClick={(e)=>{handleShow(e, d)}} >Edit </button>
+                  <button className="btn btn-danger" onClick={(e)=>deletetrainer(e,d._id)} >Delete</button>
+                </td>
+               
+                <td>{d.name}</td>             
+                <td>{d.email}</td>
+                <td>{d.password}</td>
+              </tr>
+            )
+          })
+        }
+        </tbody>
+      </Table>
+      </div>
+    </div>
+  
+      
+     <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Admin</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Container>
+          <Form>
+          <Form.Control
+                type="hidden"
+                value={data._id}
+                placeholder=""
+                id='_id'
+                name='_id'
+                onChange={(e)=>handleChange(e) }
+              />
+            <Row>
+              <Col md={6}>
+            <Form.Group className="mb-3">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                id='name'
+                name='name'
+                value={data.name}
+                onChange={(e)=>handleChange(e) }
+                placeholder=""
+                autoFocus
+              />
+            </Form.Group>
+            </Col>
+            <Col md={6}>
+            <Form.Group className="mb-3">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                id='email'
+                type="email"
+                name='email'
+                onChange={(e)=>handleChange(e) }
+                value={data.email}
+                placeholder="name@example.com"
+                autoFocus
+              />
+            </Form.Group>
+            </Col>
+            </Row>
+            <Row>
+            <Col md={6}>
+            
+            </Col>
+            <Col md={6}>
+          
+            </Col>
+            </Row>
+            <Form.Group className="mb-3">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                value={data.password}
+                type="text"
+                id='password'
+                name='password'
+                placeholder=""
+                onChange={(e)=>handleChange(e) }
+                autoFocus
+              />
+            </Form.Group>
+                      
+          </Form>
+        </Container>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={(e)=>handleSubmit(e)}>
+            Save
+          </Button>
+        </Modal.Footer>
+      </Modal>      
+
+    </div>
+  )
+}
+
+export default Admin
